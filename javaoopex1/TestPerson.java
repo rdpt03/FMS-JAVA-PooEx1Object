d008697d0b59ca0df8dc8177721967109b2add5e
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-11-12 13:48:08 +0100

    EX:1.8 and 1.9 add Person add Person class and attributes ex : 1.8 add oneToMany connection from Person to City (only Person have City, City don't have any person, but can be in more than one person)

---
+++ javaoopex1/TestPerson.java
@@ -0,0 +1,15 @@
+package fr.fms.javaoopex1;
+
+import java.util.ArrayList;
+
+public class TestPerson {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		//--------------person--------------
+		Person manu = new Person("Macron","Emmanuel",43,"Elisée à Paris", new City("Amiens","France"));
+		Person putin = new Person("Putin","Vladimir",68);
+		Person biden = new Person("Biden", "Sleepy joe");
+	}
+
+}
