c0aeda4ef2c3272eed556da23e38fb515c45a7df
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-11-13 11:43:14 +0100

    add javadoc

--- javaoopex1/City.java
+++ javaoopex1/City.java
@@ -1,101 +1,170 @@
 package fr.fms.javaoopex1;
 
+/**
+ * Represents a city with a name, country, and number of inhabitants.
+ * 
+ * This class allows you to create city instances with different levels of information,
+ * modify their attributes, and keep track of the total number of cities created.
+ * It also provides utility methods to display the city and update its population.
+ */
 public class City {
-	/*
-	public String nVille;
-	public String nPays;
-	public int habitants;
-	*/
-	private String cityName;
-	private String countryName;
-	private int inhabitants;
-	private static int citiesCount = 0;
-	
-	//---------constructors----------
-	public City(String nVille, String nPays, int habitants) throws Exception {
-		if(habitants < 0) {
-			throw new Exception("La ville "+ nVille +" ne peut contenir moins de 0 habitants");
-		}
-		this.cityName = nVille;
-		this.countryName = nPays;
-		this.inhabitants = habitants;
-		City.citiesCount++;
-	}
-	
-	public City(String nVille, int habitants) throws Exception{
-		if(habitants < 0) {
-			throw new Exception("La ville "+ nVille +" ne peut contenir moins de 0 habitants");
-		}	
-		this.cityName = nVille;
-		this.countryName = "unknown";
-		this.inhabitants = habitants;
-		City.citiesCount++;
-	}
-	
-	public City(String nVille, String country) throws Exception{
-		if(inhabitants < 0) {
-			throw new Exception("La ville "+ nVille +" ne peut contenir moins de 0 habitants");
-		}	
-		this.cityName = nVille;
-		this.countryName = country;
-		this.inhabitants = 0;
-		City.citiesCount++;
-	}
-	
 
-	//----------------------getters and setters---------------------
-	public String getCityName() {
-		return cityName;
-	}
+    private String cityName;
+    private String countryName;
+    private int inhabitants;
+    private static int citiesCount = 0;
 
-	public void setCityName(String cityName) {
-		this.cityName = cityName;
-	}
+    //---------constructors----------
 
+    /**
+     * Constructs a city with a specified name, country, and number of inhabitants.
+     *
+     * @param nVille     the name of the city
+     * @param nPays      the name of the country
+     * @param habitants  the number of inhabitants (must be ≥ 0)
+     * @throws Exception if the number of inhabitants is negative
+     */
+    public City(String nVille, String nPays, int habitants) throws Exception {
+        if(habitants < 0) {
+            throw new Exception("La ville " + nVille + " ne peut contenir moins de 0 habitants");
+        }
+        this.cityName = nVille;
+        this.countryName = nPays;
+        this.inhabitants = habitants;
+        City.citiesCount++;
+    }
 
-	public String getCountryName() {
-		return countryName;
-	}
+    /**
+     * Constructs a city with a specified name and number of inhabitants.
+     * The country is set to "unknown" by default.
+     *
+     * @param nVille     the name of the city
+     * @param habitants  the number of inhabitants (must be ≥ 0)
+     * @throws Exception if the number of inhabitants is negative
+     */
+    public City(String nVille, int habitants) throws Exception {
+        if(habitants < 0) {
+            throw new Exception("La ville " + nVille + " ne peut contenir moins de 0 habitants");
+        }
+        this.cityName = nVille;
+        this.countryName = "unknown";
+        this.inhabitants = habitants;
+        City.citiesCount++;
+    }
 
-	public void setCountryName(String countryName) {
-		this.countryName = countryName;
-	}
+    /**
+     * Constructs a city with a specified name and country.
+     * The number of inhabitants is initialized to 0.
+     *
+     * @param nVille  the name of the city
+     * @param country the name of the country
+     * @throws Exception if the initial number of inhabitants would be invalid
+     */
+    public City(String nVille, String country) throws Exception {
+        if(inhabitants < 0) {
+            throw new Exception("La ville " + nVille + " ne peut contenir moins de 0 habitants");
+        }
+        this.cityName = nVille;
+        this.countryName = country;
+        this.inhabitants = 0;
+        City.citiesCount++;
+    }
 
+    //----------------------getters and setters---------------------
 
-	public int getInhabitants() {
-		return inhabitants;
-	}
+    /**
+     * Returns the name of the city.
+     *
+     * @return the city name
+     */
+    public String getCityName() {
+        return cityName;
+    }
 
-	public void setInhabitants(int habitants) throws Exception {
-		if(habitants < 0) {
-			throw new Exception("La ville"+ this.countryName +"ne peut contenir moins de 0 habitants");
-		}
-		this.inhabitants = habitants;
-	}
-	
-	public void addHabitants(int inhabitantsToAdd) throws Exception {
-		this.setInhabitants(this.getInhabitants() + inhabitantsToAdd);
-	}
+    /**
+     * Sets or updates the name of the city.
+     *
+     * @param cityName the new city name
+     */
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
 
-	
-	
-	
+    /**
+     * Returns the name of the country the city belongs to.
+     *
+     * @return the country name
+     */
+    public String getCountryName() {
+        return countryName;
+    }
 
-	public static int getCitiesQuantity() {
-		return citiesCount;
-	}
+    /**
+     * Sets or updates the country name for the city.
+     *
+     * @param countryName the new country name
+     */
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
 
-	//ex1.4
-	public void display() {
-		System.out.println(this.toString());
-	}
-	
-	
-	//ex1.6
-	@Override
-	public String toString() {
-	    return "Ville de " + this.cityName + " en/au " + this.countryName +
-	           (this.inhabitants > 0 ? " ayant " + this.inhabitants + " habitants" : "");
-	}
+    /**
+     * Returns the number of inhabitants of the city.
+     *
+     * @return the population of the city
+     */
+    public int getInhabitants() {
+        return inhabitants;
+    }
 
+    /**
+     * Updates the number of inhabitants for the city.
+     *
+     * @param habitants the new population (must be ≥ 0)
+     * @throws Exception if the population is negative
+     */
+    public void setInhabitants(int habitants) throws Exception {
+        if(habitants < 0) {
+            throw new Exception("La ville " + this.countryName + " ne peut contenir moins de 0 habitants");
+        }
+        this.inhabitants = habitants;
+    }
+
+    /**
+     * Adds inhabitants to the current population.
+     *
+     * @param inhabitantsToAdd the number of inhabitants to add
+     * @throws Exception if the resulting population would be negative
+     */
+    public void addHabitants(int inhabitantsToAdd) throws Exception {
+        this.setInhabitants(this.getInhabitants() + inhabitantsToAdd);
+    }
+
+    /**
+     * Returns the total number of City instances created.
+     *
+     * @return the number of cities
+     */
+    public static int getCitiesQuantity() {
+        return citiesCount;
+    }
+
+    /**
+     * Displays the city information to standard output.
+     */
+    public void display() {
+        System.out.println(this.toString());
+    }
+
+    /**
+     * Returns a readable string representation of the city,
+     * including its name, country, and population if greater than 0.
+     *
+     * @return the string representation of the city
+     */
+    @Override
+    public String toString() {
+        return "Ville de " + this.cityName + " en/au " + this.countryName +
+               (this.inhabitants > 0 ? " ayant " + this.inhabitants + " habitants" : "");
+    }
 }
--- javaoopex1/Person.java
+++ javaoopex1/Person.java
@@ -1,126 +1,172 @@
 package fr.fms.javaoopex1;
 
+/**
+ * Represents a person with basic information such as last name, first name, age, address, and city of birth.
+ * Provides multiple constructors for flexibility, as well as standard getters and setters.
+ */
 public class Person {
-	private String lastName;
-	private String firstName;
-	private int age;
-	private String adress;
-	private City bornCity;
-	
-	
-	
-	public Person(String lastName, String firstName, int age, String adress, City bornCity) {
-		super();
-		this.lastName = lastName;
-		this.firstName = firstName;
-		this.age = age;
-		this.adress = adress;
-		this.bornCity = bornCity;
-	}
-	
-	
-	public Person(String nom, String prenom, int age, String adresse) {
-		super();
-		this.lastName = nom;
-		this.firstName = prenom;
-		this.age = age;
-		this.adress = adresse;
-	}
-	
-	
-	public Person(String nom, String prenom, int age) {
-		super();
-		this.lastName = nom;
-		this.firstName = prenom;
-		this.age = age;
-	}
-	
-	
-	public Person(String nom, String prenom) {
-		super();
-		this.lastName = nom;
-		this.firstName = prenom;
-	}
+    private String lastName;
+    private String firstName;
+    private int age;
+    private String adress;
+    private City bornCity;
 
+    /**
+     * Constructs a person with full information including city of birth.
+     *
+     * @param lastName  the person's last name
+     * @param firstName the person's first name
+     * @param age       the person's age
+     * @param adress    the person's address
+     * @param bornCity  the city where the person was born
+     */
+    public Person(String lastName, String firstName, int age, String adress, City bornCity) {
+        super();
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.age = age;
+        this.adress = adress;
+        this.bornCity = bornCity;
+    }
 
-	/**
-	 * @return the nom
-	 */
-	public String getNom() {
-		return lastName;
-	}
-	/**
-	 * @param nom the nom to set
-	 */
-	public void setNom(String nom) {
-		this.lastName = nom;
-	}
-	
-	
-	/**
-	 * @return the prenom
-	 */
-	public String getPrenom() {
-		return firstName;
-	}
-	/**
-	 * @param prenom the prenom to set
-	 */
-	public void setPrenom(String prenom) {
-		this.firstName = prenom;
-	}
-	
-	
-	/**
-	 * @return the age
-	 */
-	public int getAge() {
-		return age;
-	}
-	/**
-	 * @param age the age to set
-	 */
-	public void setAge(int age) {
-		this.age = age;
-	}
-	
-	
-	/**
-	 * @return the adresse
-	 */
-	public String getAdresse() {
-		return adress;
-	}
-	/**
-	 * @param adresse the adresse to set
-	 */
-	public void setAdresse(String adresse) {
-		this.adress = adresse;
-	}
+    /**
+     * Constructs a person with last name, first name, age, and address.
+     *
+     * @param nom     the person's last name
+     * @param prenom  the person's first name
+     * @param age     the person's age
+     * @param adresse the person's address
+     */
+    public Person(String nom, String prenom, int age, String adresse) {
+        super();
+        this.lastName = nom;
+        this.firstName = prenom;
+        this.age = age;
+        this.adress = adresse;
+    }
 
+    /**
+     * Constructs a person with last name, first name, and age.
+     *
+     * @param nom    the person's last name
+     * @param prenom the person's first name
+     * @param age    the person's age
+     */
+    public Person(String nom, String prenom, int age) {
+        super();
+        this.lastName = nom;
+        this.firstName = prenom;
+        this.age = age;
+    }
 
-	/**
-	 * @return the bornCity
-	 */
-	public City getBornCity() {
-		return bornCity;
-	}
+    /**
+     * Constructs a person with only last name and first name.
+     *
+     * @param nom    the person's last name
+     * @param prenom the person's first name
+     */
+    public Person(String nom, String prenom) {
+        super();
+        this.lastName = nom;
+        this.firstName = prenom;
+    }
 
+    /**
+     * Returns the last name of the person.
+     *
+     * @return the last name
+     */
+    public String getNom() {
+        return lastName;
+    }
 
-	/**
-	 * @param bornCity the bornCity to set
-	 */
-	public void setBornCity(City bornCity) {
-		this.bornCity = bornCity;
-	}
+    /**
+     * Sets or updates the last name of the person.
+     *
+     * @param nom the new last name
+     */
+    public void setNom(String nom) {
+        this.lastName = nom;
+    }
 
+    /**
+     * Returns the first name of the person.
+     *
+     * @return the first name
+     */
+    public String getPrenom() {
+        return firstName;
+    }
 
-	@Override
-	public String toString() {
-		return lastName + ", "+ firstName + ", "+ age + " ans, habitant " + adress + ", ville de naissance : " + bornCity;
-	}
-	
-	
-	
-	
-}
+    /**
+     * Sets or updates the first name of the person.
+     *
+     * @param prenom the new first name
+     */
+    public void setPrenom(String prenom) {
+        this.firstName = prenom;
+    }
+
+    /**
+     * Returns the age of the person.
+     *
+     * @return the age in years
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Sets or updates the age of the person.
+     *
+     * @param age the new age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * Returns the address of the person.
+     *
+     * @return the address
+     */
+    public String getAdresse() {
+        return adress;
+    }
+
+    /**
+     * Sets or updates the address of the person.
+     *
+     * @param adresse the new address
+     */
+    public void setAdresse(String adresse) {
+        this.adress = adresse;
+    }
+
+    /**
+     * Returns the city where the person was born.
+     *
+     * @return the birthplace city
+     */
+    public City getBornCity() {
+        return bornCity;
+    }
+
+    /**
+     * Sets or updates the city where the person was born.
+     *
+     * @param bornCity the new birthplace city
+     */
+    public void setBornCity(City bornCity) {
+        this.bornCity = bornCity;
+    }
+
+    /**
+     * Returns a readable string representation of the person,
+     * including last name, first name, age, address, and birthplace.
+     *
+     * @return a string describing the person
+     */
+    @Override
+    public String toString() {
+        return lastName + ", " + firstName + ", " + age + " ans, habitant " + adress + ", v
